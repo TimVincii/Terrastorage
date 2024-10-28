b114403f66736d00e1567d85c0ed82ebc070272d
Tim <93590862+TimVincii@users.noreply.github.com>
2024-10-28 18:15:45 +0200

    Update to 1.1.2
    This update fixes a bug with item sorting, for more information please take a look at the release notes.

---
+++ src/main/java/me/timvinci/terrastorage/item/StackIdentifier.java
@@ -0,0 +1,19 @@
+package me.timvinci.terrastorage.item;
+
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.MergedComponentMap;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Compact record used for identifying item stacks by both their item and nbt data.
+ * @param item The item of the stack.
+ * @param components The component data of the stack.
+ */
+public record StackIdentifier(Item item, @Nullable ComponentMap components) {
+
+    public StackIdentifier(ItemStack stack) {
+        this(stack.getItem(), new MergedComponentMap(stack.getComponents()));
+    }
+}
