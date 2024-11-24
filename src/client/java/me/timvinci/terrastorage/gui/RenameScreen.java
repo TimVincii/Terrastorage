135f3d37b930900f852c40b17633d3938afd7cd3
Tim <93590862+TimVincii@users.noreply.github.com>
2024-11-24 21:49:36 +0200

    Set name field focus on initialization
    Set the name TextFieldWidget in the rename screen to be focused upon the initialization of the screen.

--- src/client/java/me/timvinci/terrastorage/gui/RenameScreen.java
+++ src/client/java/me/timvinci/terrastorage/gui/RenameScreen.java
@@ -33,10 +33,11 @@ public class RenameScreen extends Screen {
         int renameButtonWidth = 150;
         int height = 20;
 
+        // Name field widget.
         int textFieldX = (this.width - (textFieldWidth + 5 + resetButtonWidth)) / 2;
         int textFieldY = (this.height - height) / 2;
         TextFieldWidget nameTextField = new TextFieldWidget(
-            textRenderer,
+            this.textRenderer,
             textFieldX,
             textFieldY,
             textFieldWidth,
@@ -44,8 +45,11 @@ public class RenameScreen extends Screen {
             Text.empty());
         nameTextField.setMaxLength(25);
         nameTextField.setText(currentName);
-        this.addDrawableChild(nameTextField);
 
+        this.addDrawableChild(nameTextField);
+        this.setFocused(nameTextField);
+
+        // Reset button widget.
         ButtonWidget resetButtonWidget = ButtonWidget.builder(
             Text.translatable("terrastorage.button.reset"),
             onPress -> {
@@ -57,6 +61,7 @@ public class RenameScreen extends Screen {
         resetButtonWidget.setTooltip(Tooltip.of(Text.translatable("terrastorage.button.tooltip.reset")));
         this.addDrawableChild(resetButtonWidget);
 
+        // Rename button widget.
         int renameButtonX = (this.width - renameButtonWidth) / 2;
         int renameButtonY = this.height - 40;
         ButtonWidget renameButtonWidget = ButtonWidget.builder(
