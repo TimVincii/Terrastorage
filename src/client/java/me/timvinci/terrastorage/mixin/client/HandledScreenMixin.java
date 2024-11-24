27d0dc418f70370baf265914c01eb3a9f579b294
Tim <93590862+TimVincii@users.noreply.github.com>
2024-11-24 21:20:26 +0200

    Add sort inventory keybind (#13)
    Added the suggested keybind for sorting the inventory of a hovered slot.

--- src/client/java/me/timvinci/terrastorage/mixin/client/HandledScreenMixin.java
+++ src/client/java/me/timvinci/terrastorage/mixin/client/HandledScreenMixin.java
@@ -18,7 +18,6 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.InputUtil;
@@ -31,6 +30,7 @@ import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -57,6 +57,9 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen
     @Shadow protected int backgroundHeight;
     @Shadow protected int x;
     @Shadow protected int y;
+    @Shadow @Nullable
+    protected Slot focusedSlot;
+    
 
     protected HandledScreenMixin(Text title) {
         super(title);
@@ -186,11 +189,41 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen
     }
 
     /**
+     * Provides the ability to sort inventories through the sort inventory keybind.
+     * Injected at TAIL to allow any other logic related to the same keybind to happen before the sorting.
+     */
+    @Inject(method = "mouseClicked", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    private void mouseClickedTail(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir, boolean bl, Slot slot) {
+        if (slot == null || slot.inventory.size() < 27) {
+            return;
+        }
+
+        if (TerrastorageKeybindings.sortInventoryBind.matchesMouse(button)) {
+            ClientNetworkHandler.sendSortPacket(slot.inventory instanceof PlayerInventory);
+        }
+    }
+
+    /**
      * Calls the ScreenInteractionUtils to process a slot click.
      */
     @Inject(method = "onMouseClick(Lnet/minecraft/screen/slot/Slot;IILnet/minecraft/screen/slot/SlotActionType;)V", at = @At("HEAD"), cancellable = true)
     private void onMouseClick(Slot slot, int slotId, int button, SlotActionType actionType, CallbackInfo ci) {
         ScreenInteractionUtils.processSlotClick(this.client, handler.getCursorStack(), slot, slotId, button, actionType, ci);
+    }
+
+    /**
+     * Provides the ability to sort inventories through the sort inventory keybind.
+     * Injected at TAIL to allow any other logic related to the same keybind to happen before the sorting.
+     */
+    @Inject(method = "keyPressed", at = @At("TAIL"))
+    private void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        if (focusedSlot == null || focusedSlot.inventory.size() < 27) {
+            return;
+        }
+
+        if (TerrastorageKeybindings.sortInventoryBind.matchesKey(keyCode, scanCode)) {
+            ClientNetworkHandler.sendSortPacket(focusedSlot.inventory instanceof PlayerInventory);
+        }
     }
 
     /**
