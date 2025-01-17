48f6856f1d6f077d9ed64df8e9df183410391dea
Tim <93590862+TimVincii@users.noreply.github.com>
2025-01-17 22:19:54 +0200

    Add new button textures (#16)
    Added new default textures for the inventory storage buttons and an option to toggle between the old and new ones.

--- src/main/java/me/timvinci/terrastorage/config/BaseConfigManager.java
+++ src/main/java/me/timvinci/terrastorage/config/BaseConfigManager.java
@@ -91,7 +91,7 @@ public abstract class BaseConfigManager<T> {
                 }
             }
             else {
-                logger.error("Failed to set field '{}' as accessible, please submit a github bug report about this.", field.getName());
+                logger.error("Failed to set field '{}' as accessible.", field.getName());
             }
         }
     }
@@ -146,7 +146,7 @@ public abstract class BaseConfigManager<T> {
                 }
             }
             else {
-                logger.error("Failed to set field '{}' as accessible, please submit a github bug report about this.", field.getName());
+                logger.error("Failed to set field '{}' as accessible.", field.getName());
                 if (hasNoErrors) {
                     hasNoErrors = false;
                 }
