09c8303432764675bfbdde7a99a62b3caa2c144f
Tim <93590862+TimVincii@users.noreply.github.com>
2025-02-21 15:55:41 +0200

    Adjust toInitialChunkDataNbt handling
    Moved the logic for adding a lockable container's custom name to its initial chunk NBT data. This is now done via a BlockEntity mixin without overriding the method, preventing potential incompatibilities with other mods as seen in #31.

---
+++ src/main/java/me/timvinci/terrastorage/mixin/BlockEntityMixin.java
@@ -0,0 +1,32 @@
+package me.timvinci.terrastorage.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.LockableContainerBlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+/**
+ * A mixin of the BlockEntity class, used for adding the custom name of lockable container block entities to
+ * their initial chunk data.
+ */
+@Mixin(BlockEntity.class)
+public abstract class BlockEntityMixin {
+
+    /**
+     * Add the custom name to the initial chunk nbt data.
+     */
+    @ModifyReturnValue(method = "toInitialChunkDataNbt", at = @At("RETURN"))
+    private NbtCompound toInitialChunkDataNbt(NbtCompound original) {
+        if ((BlockEntity) (Object) this instanceof LockableContainerBlockEntity lockableContainerBlockEntity) {
+            Text customName = lockableContainerBlockEntity.getCustomName();
+            if (customName != null) {
+                original.putString("CustomName", Text.Serializer.toJson(customName));
+            }
+        }
+
+        return original;
+    }
+}
