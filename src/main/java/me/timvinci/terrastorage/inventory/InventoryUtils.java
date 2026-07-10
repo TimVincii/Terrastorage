896e8aad9918076e48ef7dde32203d910c07a706
Tim <93590862+TimVincii@users.noreply.github.com>
2026-07-10 16:02:47 +0300

    Merge pull request #64 from BoyAxl/fix/respect-container-placement-rules
    🧰 Respect container placement rules during transfers

--- src/main/java/me/timvinci/terrastorage/inventory/InventoryUtils.java
+++ src/main/java/me/timvinci/terrastorage/inventory/InventoryUtils.java
@@ -59,8 +59,12 @@ public class InventoryUtils {
             return;
         }
 
-        if (!receiverState.getEmptySlots().isEmpty()) {
+        while (!stack.isEmpty() && !receiverState.getEmptySlots().isEmpty()) {
             int emptySlot = receiverState.getEmptySlots().poll();
+            if (!to.getItem(emptySlot).isEmpty() || !to.canPlaceItem(emptySlot, stack)) {
+                continue;
+            }
+
             to.setItem(emptySlot, stack.copyAndClear());
             receiverState.setModified();
             // Check if the stack that was transferred isn't full.
@@ -68,6 +72,8 @@ public class InventoryUtils {
                 // Add this slot to the item slots of the receiver state.
                 receiverState.getNonFullItemSlots().computeIfAbsent(stackIdentifier, k -> new ArrayList<>()).add(emptySlot);
             }
+
+            break;
         }
     }
 
@@ -87,6 +93,10 @@ public class InventoryUtils {
         while (slotsIterator.hasNext() && !stackToTransfer.isEmpty()) {
             int slotWithItem = slotsIterator.next();
             ItemStack existingStack = to.getItem(slotWithItem);
+            if (!to.canPlaceItem(slotWithItem, stackToTransfer)) {
+                slotsIterator.remove();
+                continue;
+            }
 
             int spaceLeft = existingStack.getMaxStackSize() - existingStack.getCount();
             int transferAmount;
