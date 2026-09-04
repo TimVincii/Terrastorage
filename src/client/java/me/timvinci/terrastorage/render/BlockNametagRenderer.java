e1f68dec61f16a5e7f9ad3a217561b608bb32a04
Tim <93590862+TimVincii@users.noreply.github.com>
2026-09-04 22:35:55 +0300

    Provide nametag renderers through render dispatcher
    Nametag renderers were registered at client startup: this process required creating block entities to validate they actually need a nametag renderer registered to them, and creating block entities right on client startup caused crashes (For instance, with Traveler's Backpack).
    
    The check now happens in a mixin of BlockEntityRenderDispatcher.getRenderer.
    It receives block entities so nothing has to be created, solving the crash.
    The inventory size check is also made per block entity rather than per type, which is more accurate for storages that can be upgraded,

--- src/client/java/me/timvinci/terrastorage/render/BlockNametagRenderer.java
+++ src/client/java/me/timvinci/terrastorage/render/BlockNametagRenderer.java
@@ -3,20 +3,20 @@ package me.timvinci.terrastorage.render;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
-import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.world.phys.Vec3;
 
 /**
  * An empty custom block entity renderer.
- * Any block entity that has this renderer registered to it, will then be passed to the BlockEntityRenderDispatcher for
- * rendering, which will in turn render the nametag for the block entity.
+ * Any block entity that has this renderer returned for it will be processed by the BlockEntityRenderDispatcher, which
+ * will in turn give the NametagRenderer a chance to render a nametag for it.
+ * A single instance is shared.
  */
 public class BlockNametagRenderer implements BlockEntityRenderer<RandomizableContainerBlockEntity> {
+    public static final BlockNametagRenderer INSTANCE = new BlockNametagRenderer();
 
-    public BlockNametagRenderer(BlockEntityRendererProvider.Context ctx) {}
+    private BlockNametagRenderer() {}
 
     @Override
-    public void render(RandomizableContainerBlockEntity entity, float tickProgress, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay, Vec3 cameraPos) {
-    }
+    public void render(RandomizableContainerBlockEntity entity, float tickProgress, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay, Vec3 cameraPos) {}
 }
